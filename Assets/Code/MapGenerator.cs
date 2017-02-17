bb810afde665bb522ebcd6baa9bf8c2b5658d208
Nejc Anclin <nejc.anclin@gmail.com>
2017-02-17 23:08:02 +0100

    Method returns map array.

--- Assets/Code/MapGenerator.cs
+++ Assets/Code/MapGenerator.cs
@@ -9,20 +9,19 @@ public class MapGenerator : MonoBehaviour {
     [Range(0, 1)] [SerializeField] private float Treshold;
     [SerializeField] public bool AutoUpdate;
 
-    private float[,] Map;
-
-    public void GenerateMap() {
-        Map = new float[Width, Height];
+    public float[,] GenerateMap() {
+        float[,] map = new float[Width, Height];
 
         for (int y = 0; y < Height; y++) {
             for (int x = 0; x < Width; x++) {
-                Map[x, y] = InputTexture.GetPixel(x, y).grayscale < Treshold ? 1 : 0;
+                map[x, y] = InputTexture.GetPixel(x, y).grayscale < Treshold ? 1 : 0;
             }
         }
+        return map;
     }
 
     public void RenderMap() {
-        GenerateMap();
-        MapRenderer.RenderHeightMap(Map);
+        float[,] map = GenerateMap();
+        MapRenderer.RenderHeightMap(map);
     }
 }
